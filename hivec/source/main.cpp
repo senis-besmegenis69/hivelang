0d1128d09fbc1f3b78f92a95ac5c4d20129bfcd6
Joris Baranauskas <jorisb@pop-os.localdomain>
2022-09-05 01:14:41 +0300

    Switched to c++ for llvm from c and qbe / bison / flex / lex

---
+++ hivec/source/main.cpp
@@ -0,0 +1,19 @@
+
+/**
+ * @file main.cpp
+ *
+ * @copyright This file is a part of the project hivelang and is distributed under MIT license that
+ * should have been included with the project. If not, see https://choosealicense.com/licenses/mit/
+ *
+ * @author jorisb
+ *
+ * @date 2022-09-05
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello, Hivec!\n";
+	return 0;
+}
